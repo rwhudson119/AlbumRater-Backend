d787d21724344aa38e7469543de4c9a82a1a2dae
rwhudson119 <rwhudson@student.unimelb.edu.au>
2022-07-28 16:14:04 +1000

    removed web vitals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import reportWebVitals from './reportWebVitals';
-
+//import reportWebVitals from './reportWebVitals';
+console.log('Hello, world');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
